cc7c594d9ec2d421da448b447f16faa1c793d49b
Elvia <elvia.franco@icloud.com>
2024-11-15 18:52:15 -0700

    Update

--- .next/server/chunks/ssr/_a634b4._.js
+++ .next/server/chunks/ssr/_a634b4._.js
@@ -47,6 +47,26 @@ function Header({ title, name, subtext }) {
     }, this);
 }
 }}),
+"[project]/src/app/ui/components/journal.tsx [app-rsc] (ecmascript)": ((__turbopack_context__) => {
+"use strict";
+
+var { r: __turbopack_require__, f: __turbopack_module_context__, i: __turbopack_import__, s: __turbopack_esm__, v: __turbopack_export_value__, n: __turbopack_export_namespace__, c: __turbopack_cache__, M: __turbopack_modules__, l: __turbopack_load__, j: __turbopack_dynamic__, P: __turbopack_resolve_absolute_path__, U: __turbopack_relative_url__, R: __turbopack_resolve_module_id_path__, b: __turbopack_worker_blob_url__, g: global, __dirname, x: __turbopack_external_require__, y: __turbopack_external_import__, z: require } = __turbopack_context__;
+{
+__turbopack_esm__({
+    "Journal": (()=>Journal)
+});
+var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f2e$pnpm$2f$next$40$15$2e$0$2e$3_react$2d$dom$40$19$2e$0$2e$0$2d$rc$2d$66855b96$2d$20241106_react$40$19$2e$0$2e$0$2d$rc$2d$66855b96$2d$20241106_$5f$react$40$19$2e$0$2e$0$2d$rc$2d$66855b96$2d$20241106$2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__ = __turbopack_import__("[project]/node_modules/.pnpm/next@15.0.3_react-dom@19.0.0-rc-66855b96-20241106_react@19.0.0-rc-66855b96-20241106__react@19.0.0-rc-66855b96-20241106/node_modules/next/dist/server/route-modules/app-page/vendored/rsc/react-jsx-dev-runtime.js [app-rsc] (ecmascript)");
+;
+function Journal() {
+    return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f2e$pnpm$2f$next$40$15$2e$0$2e$3_react$2d$dom$40$19$2e$0$2e$0$2d$rc$2d$66855b96$2d$20241106_react$40$19$2e$0$2e$0$2d$rc$2d$66855b96$2d$20241106_$5f$react$40$19$2e$0$2e$0$2d$rc$2d$66855b96$2d$20241106$2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
+        children: "hello"
+    }, void 0, false, {
+        fileName: "[project]/src/app/ui/components/journal.tsx",
+        lineNumber: 3,
+        columnNumber: 5
+    }, this);
+}
+}}),
 "[project]/src/app/checkin/page.tsx [app-rsc] (ecmascript)": ((__turbopack_context__) => {
 "use strict";
 
@@ -290,4 +310,4 @@ var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f2e$pnpm$2f$ne
 
 };
 
-//# sourceMappingURL=_2c5f5d._.js.map
+//# sourceMappingURL=_a634b4._.js.map
--- src/app/checkin/page.tsx
+++ src/app/checkin/page.tsx
@@ -1,12 +1,15 @@
 import { Header } from "../ui/components/header";
 import { Journal } from "../ui/components/journal";
 
-
 export default function Page() {
   return (
-    <Header
-    title="Daily Check In"
-    name="Elvia"
-    subtext="take your self-care moment"/>  
-  )
+    <>
+      <Header
+        title="Daily Check In"
+        name="Elvia"
+        subtext="take your self-care moment"
+      />
+      <Journal />
+    </>
+  );
 }
